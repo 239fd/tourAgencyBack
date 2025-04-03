ae1c0444063d256b0e3997a90cb97e9b5076ae57
Pavel Karliuk <pavelkarliuk1@gmail.com>
2025-04-04 02:46:18 +0300

    Access refresh token auth complete

---
+++ src/main/java/by/bsuir/touragency/exceptions/AuthenticationException.java
@@ -0,0 +1,7 @@
+package by.bsuir.touragency.extentions;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/by/bsuir/touragency/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package by.bsuir.touragency.extentions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
